9202ae3a7b5af8f4829f33921a9805404ff09e03
Guopeng Liang <guopeng.liang@gmail.com>
2014-04-08 13:44:17 +0800

    Update.

--- www/countly.js
+++ www/countly.js
@@ -1,34 +1,25 @@
-// Countly Cordova Plugin by Guopeng Liang 02/2014
+// clobbers: navigator.countly
 
-(function(cordova) {
+var exec = require('cordova/exec');
 
-	function Countly() {}
+var countly = {
 
-	Countly.prototype.recordEvent = function(key, count, sum, successCallback, failureCallback) {
-		if (typeof sum === "undefined") {
-			return cordova.exec(successCallback, failureCallback, 'Countly', 'recordEvent', [key, count || 1]);
+	recordEvent: function(key, count, sum, success, fail) {
+		if (typeof sum === 'undefined') {
+			exec(success, fail, 'Countly', 'recordEvent', [key, count || 1]);
 		} else {
-			return cordova.exec(successCallback, failureCallback, 'Countly', 'recordEventWithSum', [key, count || 1, sum]);
+			exec(success, fail, 'Countly', 'recordEventWithSum', [key, count || 1, sum]);
 		}
-	};
+	},
 
-	Countly.prototype.recordEventWithSegmentation = function(key, segmentation, count, sum, successCallback, failureCallback) {
-		if (typeof sum === "undefined") {
-			return cordova.exec(successCallback, failureCallback, 'Countly', 'recordEventWithSegmentation', [key, segmentation, count || 1]);
+	recordEventWithSegmentation: function(key, segmentation, count, sum, success, fail) {
+		if (typeof sum === 'undefined') {
+			exec(success, fail, 'Countly', 'recordEventWithSegmentation', [key, segmentation, count || 1]);
 		} else {
-			return cordova.exec(successCallback, failureCallback, 'Countly', 'recordEventWithSegmentationAndSum', [key, segmentation, count || 1, sum]);
+			exec(success, fail, 'Countly', 'recordEventWithSegmentationAndSum', [key, segmentation, count || 1, sum]);
 		}
-	};
+	}
 
-	Countly.install = function() {
-		if (!window.plugins) {
-			window.plugins = {};
-		}
-		window.plugins.countly = new Countly();
-		//backward compatible
-		window.countly = navigator.countly = window.plugins.countly;
-	};
+};
 
-	cordova.addConstructor(Countly.install);
-
-})(window.cordova || window.Cordova || window.PhoneGap);
+module.exports = countly;
